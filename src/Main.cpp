3881217fe5d5440377f53bfaaa3cf2c3c5f23465
blurryroots <blurryroots@posteo.de>
2015-03-17 19:21:52 +0100

    obj loader

--- src/Main.cpp
+++ src/Main.cpp
@@ -21,6 +21,9 @@
 #include <glm/vector.h>
 
 //
+#include <tiny_obj_loader.h>
+
+//
 #include <FileReader.h>
 #include <FragmentShader.h>
 #include <ShaderProgram.h>
@@ -72,6 +75,9 @@ struct Model {
 	, elements (e) {
 	}
 };
+
+void
+test_obj_loader (void);
 
 //
 void
@@ -226,13 +232,15 @@ initialize (void) {
 	glGenBuffers (1, &(game_data.element_buffer));
 
 	game_data.matrices.model = glm::translate (
-		glm::mat4 (1.0f), glm::vec3 (0.0, 0.0, -6.0)
+		glm::mat4 (1.0f), glm::vec3 (0.0, 0.0, -4.0)
 	);
 	game_data.matrices.view = glm::lookat (
-		glm::vec3 (0.0, 0.0, 0.0),
+		glm::vec3 (0.0, 0.0, 2.0),
 		glm::vec3 (0.0, 0.0, -4.0),
 		glm::vec3 (0.0, 1.0, 0.0)
 	);
+
+	test_obj_loader ();
 }
 
 void
@@ -260,9 +268,9 @@ on_update (double dt) {
 		45.0f * game_data.cam_view_buffer[2] * fdt, glm::vec3 (0, 0, 1)
 	);
 
-	game_data.pitch += 45.0f * game_data.cam_view_buffer[0] * fdt;
-	game_data.yaw += 45.0f * game_data.cam_view_buffer[1] * fdt;
-	game_data.roll += 45.0f * game_data.cam_view_buffer[2] * fdt;
+	game_data.pitch += 90.0f * game_data.cam_view_buffer[0] * fdt;
+	game_data.yaw += 90.0f * game_data.cam_view_buffer[1] * fdt;
+	game_data.roll += 90.0f * game_data.cam_view_buffer[2] * fdt;
 }
 
 void
@@ -483,4 +491,63 @@ on_mouse_button (GLFWwindow *window, int button, int action, int mods) {
 void
 on_scroll (GLFWwindow *window, double xoffset, double yoffset) {
 	//
+}
+
+void
+test_obj_loader (void) {
+	std::string inputfile = "models/box.obj";
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+
+	std::string err = tinyobj::LoadObj(shapes, materials, inputfile.c_str());
+
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+		exit(1);
+	}
+
+	std::cout << "# of shapes    : " << shapes.size() << std::endl;
+	std::cout << "# of materials : " << materials.size() << std::endl;
+
+	for (size_t i = 0; i < shapes.size(); i++) {
+		printf("shape[%ld].name = %s\n", i, shapes[i].name.c_str());
+		printf("Size of shape[%ld].indices: %ld\n", i, shapes[i].mesh.indices.size());
+		printf("Size of shape[%ld].material_ids: %ld\n", i, shapes[i].mesh.material_ids.size());
+		assert((shapes[i].mesh.indices.size() % 3) == 0);
+		for (size_t f = 0; f < shapes[i].mesh.indices.size() / 3; f++) {
+			printf("  idx[%ld] = %d, %d, %d. mat_id = %d\n", f, shapes[i].mesh.indices[3*f+0], shapes[i].mesh.indices[3*f+1], shapes[i].mesh.indices[3*f+2], shapes[i].mesh.material_ids[f]);
+		}
+
+		printf("shape[%ld].vertices: %ld\n", i, shapes[i].mesh.positions.size());
+		assert((shapes[i].mesh.positions.size() % 3) == 0);
+		for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) {
+			printf("  v[%ld] = (%f, %f, %f)\n", v,
+				shapes[i].mesh.positions[3*v+0],
+				shapes[i].mesh.positions[3*v+1],
+				shapes[i].mesh.positions[3*v+2]);
+		}
+	}
+
+	for (size_t i = 0; i < materials.size(); i++) {
+		printf("material[%ld].name = %s\n", i, materials[i].name.c_str());
+		printf("  material.Ka = (%f, %f ,%f)\n", materials[i].ambient[0], materials[i].ambient[1], materials[i].ambient[2]);
+		printf("  material.Kd = (%f, %f ,%f)\n", materials[i].diffuse[0], materials[i].diffuse[1], materials[i].diffuse[2]);
+		printf("  material.Ks = (%f, %f ,%f)\n", materials[i].specular[0], materials[i].specular[1], materials[i].specular[2]);
+		printf("  material.Tr = (%f, %f ,%f)\n", materials[i].transmittance[0], materials[i].transmittance[1], materials[i].transmittance[2]);
+		printf("  material.Ke = (%f, %f ,%f)\n", materials[i].emission[0], materials[i].emission[1], materials[i].emission[2]);
+		printf("  material.Ns = %f\n", materials[i].shininess);
+		printf("  material.Ni = %f\n", materials[i].ior);
+		printf("  material.dissolve = %f\n", materials[i].dissolve);
+		printf("  material.illum = %d\n", materials[i].illum);
+		printf("  material.map_Ka = %s\n", materials[i].ambient_texname.c_str());
+		printf("  material.map_Kd = %s\n", materials[i].diffuse_texname.c_str());
+		printf("  material.map_Ks = %s\n", materials[i].specular_texname.c_str());
+		printf("  material.map_Ns = %s\n", materials[i].normal_texname.c_str());
+		std::map<std::string, std::string>::const_iterator it(materials[i].unknown_parameter.begin());
+		std::map<std::string, std::string>::const_iterator itEnd(materials[i].unknown_parameter.end());
+		for (; it != itEnd; it++) {
+			printf("  material.%s = %s\n", it->first.c_str(), it->second.c_str());
+		}
+		printf("\n");
+	}
 }
