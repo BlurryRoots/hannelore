d677c43d875f6c86b0f78b690c2293ee7b5101ee
blurryroots <blurryroots@posteo.de>
2016-06-07 01:57:36 +0200

    changed file reader strategy

--- inc/FileReader.h
+++ inc/FileReader.h
@@ -17,39 +17,30 @@ public:
 	FileReader (std::string file_path)
 	: path (file_path)
 	, mode (std::ifstream::in)
+	, contents ()
 	, input_stream () {
 		//prepare f to throw if failbit gets set
 		std::ios_base::iostate exceptionMask =
 			this->input_stream.exceptions () | std::ios::failbit;
 		this->input_stream.exceptions (exceptionMask);
-		//
-		this->input_stream.open (this->path, this->mode);
+
+		this->input_stream.open (this->path, this->mode); {
+			std::string line;
+			while (std::getline (this->input_stream, line)) {
+				this->contents += line + "\n";
+			}
+		}
+		this->input_stream.close ();
 	}
 
 	virtual
 	~FileReader (void) {
-		this->input_stream.close ();
+		//this->input_stream.close ();
 	}
 
 	std::string
 	to_string (void) {
-		// move file pointer to end of file
-		this->input_stream.seekg (0, std::ios::end);
-		// read the file size by counting the pointer positions
-		size_t length = this->input_stream.tellg ();
-		// reset the file pointer
-		this->input_stream.seekg (0, std::ios::beg);
-		// create buffer to read in
-		char buffer[length + 1];
-		// read character until EOF
-		this->input_stream.read (buffer, length);
-		// terminate string
-		buffer[length] = '\0';
-		// reset the file pointer to be reusable
-		this->input_stream.seekg (0, std::ios::beg);
-
-		// create a string object
-		return std::string (buffer);
+		return this->contents;
 	}
 
 };
