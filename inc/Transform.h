ce9dee24e95bb9ba16c6e14eee1ce659ad0a07a5
blurryroots <blurryroots@posteo.de>
2016-07-06 05:53:55 +0200

    refactored transform class

--- inc/Transform.h
+++ inc/Transform.h
@@ -1,5 +1,5 @@
-#ifndef __Transform_h__
-#define __Transform_h__
+#ifndef hannelore_Transform_h
+#define hannelore_Transform_h
 
 // GLM
 #include <glm/glm.hpp>
@@ -13,49 +13,25 @@
 
 #include <yanecos/Data.h>
 
-class Transform : public Yanecos::Data<Transform> {
+class Transform : public blurryroots::yanecos::Data<Transform> {
 
 public:
-	static const glm::vec3 FORWARD;
-	static const glm::vec3 UP;
-	static const glm::vec3 RIGHT;
-	static const glm::vec3 ZERO;
-
-private:
-	glm::mat4 translation;
-	glm::mat4 rotation;
-	glm::mat4 scaling;
-
-public:
-	Transform (void)
-	: translation ()
-	, rotation ()
-	, scaling () {}
-
-	Transform (const Transform &other)
-	: translation (other.translation)
-	, rotation (other.rotation)
-	, scaling (other.scaling) {}
-
-	virtual
-	~Transform (void) {}
-
 	void
-	translate (const glm::vec3 &offset) {
+	translate (const glm::vec3& offset) {
 		this->translation = glm::translate (this->translation,
 			offset
 		);
 	}
 
 	void
-	scale (const glm::vec3 &factor) {
+	scale (const glm::vec3& factor) {
 		this->scaling = glm::scale (this->scaling,
 			factor
 		);
 	}
 
 	void
-	rotate (float angle, const glm::vec3 &axis) {
+	rotate (float angle, const glm::vec3& axis) {
 		this->rotation = glm::rotate (this->rotation,
 			angle, axis
 		);
@@ -69,7 +45,7 @@ public:
 	}
 
 	void
-	reset_translation () {
+	reset_translation (void) {
 		this->translation = glm::mat4 (1);
 	}
 
@@ -81,7 +57,7 @@ public:
 	}
 
 	void
-	reset_rotation () {
+	reset_rotation (void) {
 		this->rotation = glm::mat4 (1);
 	}
 
@@ -93,7 +69,7 @@ public:
 	}
 
 	void
-	reset_scale () {
+	reset_scale (void) {
 		this->scaling = glm::mat4 (1);
 	}
 
@@ -106,8 +82,27 @@ public:
 			;
 	}
 
+	Transform (void)
+	: translation ()
+	, rotation ()
+	, scaling () {}
+
+	Transform (const Transform &other)
+	: translation (other.translation)
+	, rotation (other.rotation)
+	, scaling (other.scaling) {}
+
+	virtual
+	~Transform (void) {}
+
+public:
+	static const glm::vec3 FORWARD;
+	static const glm::vec3 UP;
+	static const glm::vec3 RIGHT;
+	static const glm::vec3 ZERO;
+
 	static glm::vec3
-	to_right (const glm::mat4 &rotation) {
+	to_right (const glm::mat4& rotation) {
 		float x = rotation[0][0];
 		float y = rotation[0][1];
 		float z = rotation[0][2];
@@ -116,7 +111,7 @@ public:
 	}
 
 	static glm::vec3
-	to_up (const glm::mat4 &rotation) {
+	to_up (const glm::mat4& rotation) {
 		float x = rotation[1][0];
 		float y = rotation[1][1];
 		float z = rotation[1][2];
@@ -125,7 +120,7 @@ public:
 	}
 
 	static glm::vec3
-	to_forward (const glm::mat4 &rotation) {
+	to_forward (const glm::mat4& rotation) {
 		float x = rotation[2][0];
 		float y = rotation[2][1];
 		float z = rotation[2][2];
@@ -134,13 +129,19 @@ public:
 	}
 
 	static glm::vec3
-	to_position (const glm::mat4 &translation) {
+	to_position (const glm::mat4& translation) {
 		float x = translation[3][0];
 		float y = translation[3][1];
 		float z = translation[3][2];
 
 		return glm::vec3 (x, y, z);
 	}
+
+private:
+	glm::mat4 translation;
+	glm::mat4 rotation;
+	glm::mat4 scaling;
+
 };
 
 const glm::vec3 Transform::FORWARD = glm::vec3 (0, 0, 1);
